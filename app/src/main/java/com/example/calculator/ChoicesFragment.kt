0fe3f3e8487896f8e2c51c109d5077c397fb4458
Crucialjun <nich.otieno@gmail.com>
2020-05-09 05:44:54 +0300

    create the force fragment

--- app/src/main/java/com/example/calculator/ChoicesFragment.kt
+++ app/src/main/java/com/example/calculator/ChoicesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_choices.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,4 +21,11 @@ class ChoicesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choices, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonForce.setOnClickListener {
+            it.findNavController().navigate(R.id.action_choicesFragment_to_forceFragment)
+        }
+    }
 }

6f4c20aa102d2edce2aeee3783a2420fd40e33a8
Crucialjun <nich.otieno@gmail.com>
2020-05-11 17:55:25 +0300

    Add Force Fragment calculation

--- app/src/main/java/com/example/calculator/ForceFragment.kt
+++ app/src/main/java/com/example/calculator/ForceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_force.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,4 +20,29 @@ class ForceFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_force, container, false)
     }
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonForceSubmit.setOnClickListener {
+            val mass : Int = editTextMass.text.toString().toInt()
+            val acceleration = editTextAcceleration.text.toString().toInt()
+            var force  = calculateForce(mass,acceleration)
+
+            val formattedString = " The force exerted by a mass of  ${mass.toString()} " +
+                    "and an acceleration of ${acceleration.toString()} is equal to " +
+                    "${force.toString()} Newtons"
+
+            textViewForceResult.text = formattedString
+            textViewForceResult.visibility = View.VISIBLE
+        }
+
+
+    }
+
+    private fun calculateForce(mass: Int, acceleration: Int): Int {
+        val force = mass * acceleration
+        return force
+    }
+    }
+
+
